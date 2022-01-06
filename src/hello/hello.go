b4790d1526c98e13a96c8b2ebfa3c384cc763bec
Carlossh8 <carlos.sh8@outlook.com>
2022-01-05 23:40:53 -0300

    Alterações arquivo .go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello word")
+
+	fmt.Println("Digite seu nome: ")
+	var nome1 string
+	fmt.Scanln(&nome1)
+
+	fmt.Println("Olá," + nome1)
+	fmt.Println("Você está executando a versão 1.0 do sistema")
+
+	fmt.Println("----------------------")
+
+	fmt.Println("Iniciando Menu do sistema")
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do programa")
+	var comando int
+	fmt.Scanf("%d", &comando)
+
+	fmt.Println("O comando escolhido foi", comando)
 }
