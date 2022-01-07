0f161a416b00f7054bfcef3e11f500513032d604
Carlossh8 <carlos.sh8@outlook.com>
2022-01-06 23:56:22 -0300

    Introdução às funções

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("Digite seu nome: ")
-	var nome1 string
-	fmt.Scanln(&nome1)
-
-	fmt.Println("Olá," + nome1)
-	fmt.Println("Você está executando a versão 1.0 do sistema")
+	Introducao()
 
 	// Menu do sistema
 
@@ -19,21 +17,37 @@ func main() {
 	fmt.Println("1- Iniciar monitoramento")
 	fmt.Println("2- Exibir logs")
 	fmt.Println("0- Sair do programa")
-	var comando int
-	fmt.Scanf("%d", &comando)
 
-	fmt.Println("O comando escolhido foi", comando)
-
+	comando := lerOpcao()
 	// Escolha do usuário
 
-	switch {
+	switch comando {
 	case 1:
-		fmt.Println("Iniciando Monitoramente")
+		fmt.Println("Iniciando Monitoramento")
 	case 2:
 		fmt.Println("Iniciando Logs")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não reconheço esse comando...")
 	}
+}
+
+func Introducao() {
+
+	fmt.Println("Digite seu nome: ")
+	var nome1 string
+	fmt.Scanln(&nome1)
+
+	fmt.Println("Olá," + nome1)
+	fmt.Println("Você está executando a versão 1.0 do sistema")
+}
+
+func lerOpcao() int {
+	var comando int
+	fmt.Scanf("%d", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+
+	return comando
 }
