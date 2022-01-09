89ab475b58260338d2306cc871d8c8255899dc88
Carlossh8 <carlos.sh8@outlook.com>
2022-01-09 12:34:20 -0300

    Acremento de requisições da web.

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -10,7 +10,44 @@ func main() {
 
 	Introducao()
 
-	// Menu do sistema
+	for {
+		ApresentacaoMenu()
+
+		comando := lerOpcao()
+		// Escolha do usuário
+
+		switch comando {
+		case 1:
+			IniciarMonitoramento()
+		case 2:
+			fmt.Println("Iniciando Logs")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não reconheço esse comando...")
+		}
+	}
+}
+
+// Função responsável pela captura do nome do usuário
+// e exibir a versão do programa
+
+func Introducao() {
+
+	fmt.Println("Digite seu nome: ")
+	var nome1 string
+	fmt.Scanln(&nome1)
+	fmt.Println("-----------------------")
+
+	fmt.Println("Olá," + nome1)
+	fmt.Println("Você está executando a versão 1.1 do sistema")
+
+}
+
+// Menu do sistema
+
+func ApresentacaoMenu() {
 
 	fmt.Println("----------------------")
 
@@ -18,32 +55,9 @@ func main() {
 	fmt.Println("1- Iniciar monitoramento")
 	fmt.Println("2- Exibir logs")
 	fmt.Println("0- Sair do programa")
-
-	comando := lerOpcao()
-	// Escolha do usuário
-
-	switch comando {
-	case 1:
-		IniciarMonitoramento()
-	case 2:
-		fmt.Println("Iniciando Logs")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não reconheço esse comando...")
-	}
 }
 
-func Introducao() {
-
-	fmt.Println("Digite seu nome: ")
-	var nome1 string
-	fmt.Scanln(&nome1)
-
-	fmt.Println("Olá," + nome1)
-	fmt.Println("Você está executando a versão 1.1 do sistema")
-}
+// Responsável por ler opção digitada
 
 func lerOpcao() int {
 	var comando int
@@ -53,9 +67,16 @@ func lerOpcao() int {
 	return comando
 }
 
+// Responsável pelo monitoramente
+
 func IniciarMonitoramento() {
 	fmt.Println("Iniciando Monitoramento")
-	site := "https://www.google.com.br"
+	site := "https://github.com/"
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status code:", resp.StatusCode)
+	}
 }
